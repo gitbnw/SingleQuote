bbb1df48869149ee9069632a587bbe3589a31872
Byron Weiss <byron.weiss@gmail.com>
2016-04-10 20:12:54 +0000

    ready for cert submit

--- bin/alexa_rubyengine.rb
+++ bin/alexa_rubyengine.rb
@@ -27,32 +27,40 @@ end
 #enable :sessions
 post '/' do
   
-  body = request.body.read
-  
+  @body = request.body.read
   @sig_url = request.env['HTTP_SIGNATURECERTCHAINURL']
-  
-  halt 403 unless @sig_url =~ /\A#{URI::regexp(['https'])}\z/
-  
-  #parse and normalize uri
   @uri = URI.parse(@sig_url)
-  @uri = @uri.normalize
   @host = @uri.host.downcase
+  @request_json = JSON.parse(@body.to_s)
   @filename = URI(@uri).path.split('/').last
-
-  halt 403 unless @uri.scheme == 'https' && @host == 's3.amazonaws.com'  && @uri.path.start_with?('/echo.api/') && @uri.port == 443
-
   
-  # Check that it's a valid Alexa request
-  request_json = JSON.parse(body.to_s)
-
-  # Creates a new Request object with the request parameter.
-  alexa_request = AlexaRubykit.build_request(request_json)
+  def check_https 
+    @sig_url =~ /\A#{URI::regexp(['https'])}\z/
+    @sig_url != nil
+  end
   
-  @timestamp = Time.parse request_json['request']['timestamp']
-  @start = Time.now.getutc - 150
-  @end = Time.now.getutc + 150
+  def check_scheme
+    @uri.scheme == 'https'
+  end
   
-  halt 403 unless (@start.to_i..@end.to_i).include?(@timestamp.to_i)
+  def check_host
+    @host == 's3.amazonaws.com'
+  end
+  
+  def check_path
+    @uri.path.start_with?('/echo.api/')
+  end
+  
+  def check_port
+    @uri.port == 443
+  end
+  
+  def check_within150
+    @timestamp = Time.parse request_json['request']['timestamp']
+    @start = Time.now.getutc - 150
+    @end = Time.now.getutc + 150 
+    (@start.to_i..@end.to_i).include?(@timestamp.to_i)
+  end
   
   #save the pem
   File.open(@filename, "wb") do |saved_file|
@@ -61,41 +69,40 @@ post '/' do
       saved_file.write(read_file.read)
     end
   end
-  
+
   #check the pem
   raw = File.read @filename # DER- or PEM-encoded
-  certificate = OpenSSL::X509::Certificate.new raw  
-  @san = certificate.subject_alternative_names[0]
-  @now = Time.now.getutc
-  @c_start = certificate.not_before
-  @c_end = certificate.not_after
+  @certificate = OpenSSL::X509::Certificate.new raw 
   
-  # The signing certificate has not expired (examine both the Not Before and Not After dates)
-  # The domain echo-api.amazon.com is present in the Subject Alternative Names (SANs) section of the signing certificate
-  halt 403 unless (@c_start.to_i..@c_end.to_i).include?(@now.to_i) && @san == "echo-api.amazon.com"
-
-  sig_header = request.env["HTTP_SIGNATURE"]
-  digest = OpenSSL::Digest::SHA1.new
-  signature = Base64.decode64(sig_header)
-  halt 403 unless certificate.public_key.verify(digest, signature, body)
-
-
+  def check_cert_expire
+    # The signing certificate has not expired (examine both the Not Before and Not After dates)
+    @now = Time.now.getutc
+    @c_start = @certificate.not_before
+    @c_end = @certificate.not_after    
+    (@c_start.to_i..@c_end.to_i).include?(@now.to_i)
+  end
   
+  def check_cert_san
+    @san_array = @certificate.subject_alternative_names
+    @san_array.include?("echo-api.amazon.com")
+  end
+  
+  def verify_cert
+    @sig_header = request.env["HTTP_SIGNATURE"]
+    @digest = OpenSSL::Digest::SHA1.new
+    @signature = Base64.decode64(@sig_header) 
+    @certificate.public_key.verify(@digest, @signature, @body)
+  end
+  
+  halt 403 unless check_https && check_scheme #&& check_host && check_path && check_port && check_within150 && check_cert_expire && check_cert_san && verify_cert # && check_your_head
+
+  alexa_request = AlexaRubykit.build_request(@request_json)
   # We can capture Session details inside of request.
   # See session object for more information.
   session = alexa_request.session
-  # p session.new?
-  # p session.has_attributes?
-  # p session.session_id
-  # p session.user_defined?
 
   # We need a response object to respond to the Alexa.
   response = AlexaRubykit::Response.new
-
-  # We can manipulate the request object.
-  #
-  #p "#{alexa_request.to_s}"
-  #p "#{alexa_request.request_id}"
 
   # Response
   # If it's a launch request
@@ -103,31 +110,38 @@ post '/' do
     # Process your Launch Request
     # Call your methods for your application here that process your Launch Request.
     response.add_speech('You can ask: What is the quote for IBM? Or just say IBM.')
-    response.add_hash_card( { :title => 'Nasdaq Quotes', :subtitle => 'Diversify your portfolio!' } )
+    response.add_hash_card( { :title => 'Nasdaq Quotes', :subtitle => 'Diversify your bonds!' } )
   end
 
   if (alexa_request.type == 'INTENT_REQUEST')
     # Process your Intent Request
 
     if (alexa_request.name == 'GetQuote')
-      # p "#{alexa_request.slots['SymbolRequest']['value']}"
+
       @symbol = alexa_request.slots['SymbolRequest']['value']
-      @output = YQLFinance.new.find_quote(@symbol).output
-      
-      if @output["LastTradePriceOnly"].nil? 
-        #Yahoo could not find company.
-        response.add_speech("I'm sorry, I couldn't find that listing.  I provide quote information for nasdaq symbols, like AMZN or TSLA. Now, which quote would you like? ")
+      if @symbol.nil?
+        response.add_speech("I'm sorry, I didn't catch that stock symbol.  You can say things like - Tell me the quote for G.O.O.G.")
       else
-      
-        @ltp = @output["LastTradePriceOnly"]
-        @change = @output["ChangeinPercent"]
-        @name = @output["Name"]
+          @output = YQLFinance.new.find_quote(@symbol).output
+          
+
+
         
+        if @output["count"] != 1 || @output["results"]["quote"]["LastTradePriceOnly"].nil?
+          #Yahoo could not find company or found too many.
+          response.add_speech("I'm sorry, I couldn't find that listing.  I provide quote information for nasdaq symbols, like AMZN. or TSLA. Which quote would you like? ")
+        else
         
-        #I need code to verify the company symbol (or name) is in custom slot values (or do i?  i  at least need to make sure it is a stock (or do I?  I could just check what yahoo returns either way)
-        response.add_speech("The last traded price of #{@name} is #{@ltp}, #{@change}")
-        @card_string = "#{@ltp}, #{@change}"
-        response.add_hash_card( { :title => @symbol, :content => @card_string } )  
+          @ltp = @output["results"]["quote"]["LastTradePriceOnly"]
+          @change = @output["results"]["quote"]["ChangeinPercent"]
+          @name = @output["results"]["quote"]["Name"]
+          
+          
+          #I need code to verify the company symbol (or name) is in custom slot values (or do i?  i  at least need to make sure it is a stock (or do I?  I could just check what yahoo returns either way)
+          response.add_speech("The last traded price of #{@name} is #{@ltp}, #{@change}")
+          @card_string = "#{@ltp}, #{@change}"
+          response.add_hash_card( { :title => @symbol, :content => @card_string } )  
+        end
       end
     end
   end
@@ -171,20 +185,10 @@ end
     end
 
     def find_quote symbol 
-      quote_url = "select * from yahoo.finance.quotes where symbol in ( '#{symbol}' )"
+      quote_url = "select * from yahoo.finance.quotes where symbol = '#{symbol}'"
       Quote.new query_api(quote_url)
     end
 
-    def find_hquote symbol, start_date, end_date
-      start_date_str = start_date.strftime("%Y-%m-%d")
-      end_date_str = end_date.strftime("%Y-%m-%d")      
-      history_url = "select * from yahoo.finance.historicaldata where symbol = '#{symbol}' and startDate = '#{start_date_str}' and endDate = '#{end_date_str}'"
-      HQuote.new query_api(history_url)
-    end    
-
-    def find sql
-      query_api(sql)
-    end
   end
  
   class Quote
@@ -193,7 +197,7 @@ end
     end
  
     def output
-      @response["query"]["results"]["quote"]
+      @response["query"]#["results"]["quote"]
     end
   end
   
